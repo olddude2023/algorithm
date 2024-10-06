bac7503e9018dd6b66e09118debb89027d1611fb
olddude2023 <140768362+olddude2023@users.noreply.github.com>
2024-10-06 11:27:07 -0700

    Update FenwickTreedemo.cpp

--- FenwickTreedemo.cpp
+++ FenwickTreedemo.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-int n,m;
-int a[50005] = {0},c[50005]; //对应原数组和树状数组
+
+int n;// total number of data
+int a[50005],c[50005]; //对应原数组和树状数组
 
 int lowbit(int x){
     return x&(-x);
 }
-
-void updata(int i,int k){    //在i位置加上k
+void updata(int i,int val){    //在i位置加上k
     while(i <= n){
-        c[i] += k;
+        c[i] += val;
         i += lowbit(i);
     }
 }
 
-int getsum(int i){ //求C[1 - i]的和，即A[i]值
+int getsum(int i){        //求c from 1 to i的和
     int res = 0;
     while(i > 0){
         res += c[i];
@@ -24,18 +25,34 @@ int getsum(int i){ //求C[1 - i]的和，即A[i]值
 }
 
 int main(){
-    cin>>n;
-    for(int i = 1; i <= n; i++){
-        cin>>a[i];
-        updata(i,a[i] - a[i-1]);   //输入初值的时候，也相当于更新了值
-    }
-    
-    //[x,y]区间内加上k
-    updata(x,k);    //A[x] - A[x-1]增加k
-    updata(y+1,-k);        //A[y+1] - A[y]减少k
-    
-    //查询i位置的值
-    int sum = getsum(i);
+    int t;
+    cin>>t;
+    for(int tot = 1; tot <= t; tot++){
+        cout << "Case " << tot << ":" << endl;
+        memset(a, 0, sizeof a);
+        memset(c, 0, sizeof c);
+        cin>>n;
+        for(int i = 1; i <= n; i++){
+            cin>>a[i];
+            updata(i,a[i]);   //输入初值的时候，也相当于更新了值
+        }
 
+        string s;
+        int x,y;
+        while(cin>>s && s[0] != 'E'){
+            cin>>x>>y;
+            if(s[0] == 'Q'){    //求和操作
+                int sum = getsum(y) - getsum(x-1);    //x-y区间和也就等于1-y区间和减去1-(x-1)区间和
+                cout << sum << endl;
+            }
+            else if(s[0] == 'A'){
+                updata(x,y);
+            }
+            else if(s[0] == 'S'){
+                updata(x,-y);    //减去操作，即为加上相反数
+            }
+        }
+
+    }
     return 0;
 }
